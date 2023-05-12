3f8b4116809216df6c0b28d02979aae7e0670d42
Sourcery AI <>
2023-05-12 06:08:08 +0000

    'Refactored by Sourcery'

--- contrib/normalize_triplet.py
+++ contrib/normalize_triplet.py
@@ -6,16 +6,9 @@ import re, sys
 # a method `platform_key_abi()` to parse uname-like output into something standardized.
 
 if len(sys.argv) < 2:
-    print("Usage: {} <host triplet> [<gcc version>] [<cxxabi11>]".format(sys.argv[0]))
+    print(f"Usage: {sys.argv[0]} <host triplet> [<gcc version>] [<cxxabi11>]")
     sys.exit(1)
 
-arch_mapping = {
-    'x86_64': '(x86_|amd)64',
-    'i686': "i\\d86",
-    'aarch64': "(arm|aarch)64",
-    'armv7l': "arm(v7l)?",
-    'powerpc64le': "p(ower)?pc64le",
-}
 platform_mapping = {
     'darwin': "-apple-darwin[\\d\\.]*",
     'freebsd': "-(.*-)?freebsd[\\d\\.]*",
@@ -45,22 +38,26 @@ cxx_abi_mapping = {
 
 # Helper function to collapse dictionary of mappings down into a regex of
 # named capture groups joined by "|" operators
-c = lambda mapping: "("+"|".join(["(?P<%s>%s)"%(k,v) for (k, v) in mapping.items()]) + ")"
+c = (
+    lambda mapping: "("
+    + "|".join([f"(?P<{k}>{v})" for (k, v) in mapping.items()])
+    + ")"
+)
+arch_mapping = {
+    'x86_64': '(x86_|amd)64',
+    'i686': "i\\d86",
+    'aarch64': "(arm|aarch)64",
+    'armv7l': "arm(v7l)?",
+    'powerpc64le': "p(ower)?pc64le",
+}
 mondo_regex = re.compile(
-    "^"+
-    c(arch_mapping)+
-    c(platform_mapping)+
-    c(libc_mapping)+
-    c(call_abi_mapping)+
-    c(libgfortran_version_mapping)+
-    c(cxx_abi_mapping)+
-    "$"
+    f"^{c(arch_mapping)}{c(platform_mapping)}{c(libc_mapping)}{c(call_abi_mapping)}{c(libgfortran_version_mapping)}{c(cxx_abi_mapping)}$"
 )
 
 # Apply our mondo regex to our input:
 m = mondo_regex.match(sys.argv[1])
 if m is None:
-    print("ERROR: Unmatchable platform string '%s'!"%(sys.argv[1]))
+    print(f"ERROR: Unmatchable platform string '{sys.argv[1]}'!")
     sys.exit(1)
 
 # Helper function to find the single named field within the giant regex
@@ -101,42 +98,40 @@ def p(x):
     if x:
         for k in os_remapping:
             x = x.replace(k, os_remapping[k])
-        return '-' + x
+        return f'-{x}'
     return x
 
 # If the user passes in a GCC version (like 8.2.0) use that to force a
 # "-libgfortran5" tag at the end of the triplet, but only if it has otherwise
 # not been specified.
-if libgfortran_version == "blank_libgfortran":
-    if len(sys.argv) >= 3:
-        # If there was no gfortran/gcc version passed in, default to the latest libgfortran version
-        if not sys.argv[2]:
-            libgfortran_version = "libgfortran5"
+if libgfortran_version == "blank_libgfortran" and len(sys.argv) >= 3:
+    # If there was no gfortran/gcc version passed in, default to the latest libgfortran version
+    if not sys.argv[2]:
+        libgfortran_version = "libgfortran5"
+    else:
+        # Grab the first number in the last word with a number
+        # This will be the major version number.
+        major_ver = -1
+        words = sys.argv[2].split()
+        for word in words[::-1]:
+            major_ver = re.search("[0-9]+", word)
+            if major_ver:
+                major_ver = int(major_ver.group())
+                break
+
+        if major_ver <= 6:
+            libgfortran_version = "libgfortran3"
+        elif major_ver <= 7:
+            libgfortran_version = "libgfortran4"
         else:
-            # Grab the first number in the last word with a number
-            # This will be the major version number.
-            major_ver = -1
-            words = sys.argv[2].split()
-            for word in words[::-1]:
-                major_ver = re.search("[0-9]+", word)
-                if major_ver:
-                    major_ver = int(major_ver.group())
-                    break
+            libgfortran_version = "libgfortran5"
 
-            if major_ver <= 6:
-                libgfortran_version = "libgfortran3"
-            elif major_ver <= 7:
-                libgfortran_version = "libgfortran4"
-            else:
-                libgfortran_version = "libgfortran5"
-
-if cxx_abi == "blank_cxx_abi":
-    if len(sys.argv) == 4:
-        cxx_abi = {
-            "0": "cxx03",
-            "1": "cxx11",
-            "": "",
-        }[sys.argv[3]]
+if cxx_abi == "blank_cxx_abi" and len(sys.argv) == 4:
+    cxx_abi = {
+        "0": "cxx03",
+        "1": "cxx11",
+        "": "",
+    }[sys.argv[3]]
 
 print(arch+p(platform)+p(libc)+r(call_abi)+p(libgfortran_version)+p(cxx_abi))
 
